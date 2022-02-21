837a4f024ffcac6e179799b6edc9e62b3bdf82e8
Henrik Hjalmarsson <Serialundead@gmail.com>
2022-02-21 19:20:52 +0100

    Added login component

--- web-angular/produce-exchange-hub-web/src/app/app.module.ts
+++ web-angular/produce-exchange-hub-web/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
