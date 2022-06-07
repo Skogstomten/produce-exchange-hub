f9565dff476bd8ef0dec43e6a0d5bf4337f5fbdb
Henrik Hjalmarsson <Skogstomten@users.noreply.github.com>
2022-06-08 00:22:09 +0200

    Added some doc comments to see how pylint reacts

--- api/app/cryptography/password_hasher.py
+++ api/app/cryptography/password_hasher.py
@@ -1,3 +1,6 @@
+"""
+Contains methods for hashing, verifying and generating salt for passwords
+"""
 import base64
 
 from Crypto.Protocol.KDF import PBKDF2
@@ -6,11 +9,22 @@ from Crypto.Random import get_random_bytes
 
 
 def generate_salt() -> bytes:
+    """
+    Generates a random 256 byte long salt
+    :return:
+    """
     salt = get_random_bytes(256)
     return base64.urlsafe_b64encode(salt)
 
 
 def hash_password(password: str, salt: bytes) -> str:
+    """
+    Hashes a password using PBKDF2 algorith
+
+    :param password: password to be hashed
+    :param salt: salt to use
+    :return: hashed password with salt as base64 encoded byte string
+    """
     hashed_value = PBKDF2(password, salt, 64, 1000000, hmac_hash_module=SHA512)
     hashed_value = base64.urlsafe_b64encode(hashed_value + salt)
     hashed_value = hashed_value.decode('utf-8')
@@ -18,6 +32,12 @@ def hash_password(password: str, salt: bytes) -> str:
 
 
 def is_correct_password(in_password: str, hashed_password: str) -> bool:
+    """
+    Verifies that provided password is same as hashed password
+    :param in_password:
+    :param hashed_password:
+    :return: True of password is a match
+    """
     hashed_password_bytes = hashed_password.encode('utf-8')
     hashed_password_bytes = base64.urlsafe_b64decode(hashed_password_bytes)
     salt = hashed_password_bytes[64:]
