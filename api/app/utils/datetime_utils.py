d8ce7ab6744ae20a850ee4bc317fe69c956a6868
Henrik Hjalmarsson <henrik.hjalmarsson@payex.com>
2022-06-16 14:11:55 +0200

    More tests and docstrings.
    Removed test_datetime_utils and replaced the tests wth doctests. Added docstrings to datetime_utils.

--- api/app/utils/datetime_utils.py
+++ api/app/utils/datetime_utils.py
@@ -1,8 +1,29 @@
+"""
+Has utility functions for working with datetime objects.
+"""
 from datetime import datetime
-from pytz import utc
+from pytz import utc, timezone
 
 
-def ensure_utc(d: datetime):
-    if d.tzinfo is None or d.tzinfo.utcoffset(d) is None:
-        d = utc.localize(d)
-    return d
+def ensure_utc(date: datetime) -> datetime:
+    """
+    Makes sure datetime has timezone utc.
+
+    >>> d = ensure_utc(datetime.now())
+    >>> d.tzinfo == utc
+    True
+
+    >>> d = ensure_utc(datetime.now(utc))
+    >>> d.tzinfo == utc
+    True
+
+    >>> d = ensure_utc(datetime.now(timezone('Europe/Stockholm')))
+    >>> d.tzinfo == utc
+    True
+
+    :param date: the datetime object to be checked.
+    :return: datetime instance of the same time but ensured to be utc timezone.
+    """
+    if date.tzinfo is None or date.tzinfo.utcoffset(date) is None:
+        date = utc.localize(date)
+    return date
