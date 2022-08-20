17944ecc8d9614adfcaba5ae56ce38bc0557473e
Henrik Hjalmarsson <henrik.hjalmarsson@payex.com>
2022-08-20 16:20:57 +0200

    Added AddOrderModel

---
+++ api/app/company/models/v1/orders.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+from app.company.models.shared.enums import Currency, IntervalType
+from app.shared.models.v1.shared import Language
+
+
+class AddOrderModel(BaseModel):
+    product_id: str
+    description: dict[Language, str]
+    price: float | None
+    currency: Currency | None
+    quantity: int | None
+    recurring: bool
+    interval: int | None
+    interval_type: IntervalType | None
