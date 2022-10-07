5d5132c3899b14aa8a113256b4d307fd617d66e8
Henrik Hjalmarsson <henrik.hjalmarsson@payex.com>
2022-10-07 21:57:22 +0200

    Started building markup for registration page

--- produce-exchange-hub-web/src/ProduceExchangeHub/ProduceExchangeHub/User/Pages/Register.razor.cs
+++ produce-exchange-hub-web/src/ProduceExchangeHub/ProduceExchangeHub/User/Pages/Register.razor.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Localization;
 using ProduceExchangeHub.User.Models;
+using ProduceExchangeHub.User.Services;
 
 namespace ProduceExchangeHub.User.Pages;
 
@@ -9,5 +10,19 @@ public partial class Register
     [Inject]
     private IStringLocalizer<Register> Loc { get; set; } = null!;
 
+    [Inject]
+    private IUserService UserService { get; set; } = null!;
+
+    [Inject]
+    private NavigationManager NavManager { get; set; } = null!;
+
     private RegisterModel Model { get; set; } = new();
+
+    private async Task OnRegisterUserSubmit()
+    {
+        await UserService.RegisterUserAsync(Model);
+        Model = new RegisterModel();
+
+        NavManager.NavigateTo("/authentication/login");
+    }
 }
