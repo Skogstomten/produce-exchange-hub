65d04a8a8306db4c8a4654eda678062e4133b88d
Henrik Hjalmarsson <henrik.hjalmarsson@payex.com>
2022-10-26 19:00:43 +0200

    Added address to company page

---
+++ produce-exchange-hub-web/src/ProduceExchangeHub/ProduceExchangeHub/Company/Services/AddressService.cs
@@ -0,0 +1,18 @@
+﻿using ProduceExchangeHub.Company.Models;
+using ProduceExchangeHub.Shared.Localization.Services;
+using ProduceExchangeHub.Shared.Services;
+
+namespace ProduceExchangeHub.Company.Services;
+
+public class AddressService : ServiceBase, IAddressService
+{
+    public AddressService(HttpClient httpClient, ICultureService cultureService, ILogger<AddressService> logger)
+        : base(httpClient, cultureService, logger)
+    {
+    }
+
+    public Task<IEnumerable<AddressModel>> GetCompanyAddressesAsync(string companyID)
+    {
+        return GetAsync<IEnumerable<AddressModel>>($"companies/{companyID}/addresses/");
+    }
+}
---
+++ produce-exchange-hub-web/src/ProduceExchangeHub/ProduceExchangeHub/Company/Services/IAddressService.cs
@@ -0,0 +1,8 @@
+﻿using ProduceExchangeHub.Company.Models;
+
+namespace ProduceExchangeHub.Company.Services;
+
+public interface IAddressService
+{
+    Task<IEnumerable<AddressModel>> GetCompanyAddressesAsync(string companyID);
+}
