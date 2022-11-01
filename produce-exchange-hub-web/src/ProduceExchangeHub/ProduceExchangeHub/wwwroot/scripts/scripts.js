74dc27bc93051e4097c7db5a67af636fd915c4f2
Henrik Hjalmarsson <henrik.hjalmarsson@payex.com>
2022-11-01 15:30:18 +0100

    Fixed tab layout on admin page

--- produce-exchange-hub-web/src/ProduceExchangeHub/ProduceExchangeHub/wwwroot/scripts/scripts.js
+++ produce-exchange-hub-web/src/ProduceExchangeHub/ProduceExchangeHub/wwwroot/scripts/scripts.js
@@ -7,3 +7,11 @@ window.setFocus = (id) => {
     var element = document.getElementById(id);
     element.focus();
 };
+
+window.addClass = (element, cls) => {
+    element.classList.add(cls);
+};
+
+window.removeClass = (element, cls) => {
+    element.classList.remove(cls);
+};
