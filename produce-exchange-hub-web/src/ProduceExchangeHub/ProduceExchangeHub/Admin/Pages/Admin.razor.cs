a22b3e41761852553ed475c31c83b40c7f896310
Henrik Hjalmarsson <henrik.hjalmarsson@payex.com>
2022-11-01 11:22:39 +0100

    Added Admin page shell

---
+++ produce-exchange-hub-web/src/ProduceExchangeHub/ProduceExchangeHub/Admin/Pages/Admin.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+
+namespace ProduceExchangeHub.Admin.Pages;
+
+public partial class Admin
+{
+    [Inject]
+    public IStringLocalizer<Admin> Loc { get; set; } = null!;
+}
