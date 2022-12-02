9067daf629321c375b140df89045d0aa1795827c
Henrik Hjalmarsson <henrik.hjalmarsson@payex.com>
2022-12-02 16:42:32 +0100

    Added models
    Ran migration and added models to admin interface

--- farmers-market/farmers_market/main/admin.py
+++ farmers-market/farmers_market/main/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.site.register(models.Language)
+admin.site.register(models.CompanyType)
+admin.site.register(models.CompanyStatus)
+admin.site.register(models.ChangeType)
+admin.site.register(models.Company)
+admin.site.register(models.CompanyRole)
+admin.site.register(models.CompanyUser)
+admin.site.register(models.CompanyChange)
+admin.site.register(models.CompanyNames)
+admin.site.register(models.CompanyDescriptions)
+admin.site.register(models.Country)
+admin.site.register(models.Address)
+admin.site.register(models.ContactType)
+admin.site.register(models.Contact)
+admin.site.register(models.Product)
+admin.site.register(models.ProductName)
+admin.site.register(models.Currency)
+admin.site.register(models.Order)
--- farmers-market/farmers_market/main/models.py
+++ farmers-market/farmers_market/main/models.py
@@ -1,3 +1,112 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Language(models.Model):
+    iso_639_1 = models.CharField(max_length=2)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.iso_639_1}: {self.name}"
+
+
+class CompanyType(models.Model):
+    type_name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+
+
+class CompanyStatus(models.Model):
+    status_name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+
+
+class ChangeType(models.Model):
+    change_type = models.CharField(max_length=20)
+
+
+class Company(models.Model):
+    status = models.ForeignKey(CompanyStatus, on_delete=models.PROTECT)
+    created_date = models.DateTimeField(auto_now_add=True)
+    company_types = models.ManyToManyField(CompanyType, related_name="companies")
+    content_languages = models.ManyToManyField(Language, related_name="companies")
+    activation_date = models.DateTimeField(null=True, default=None)
+    external_website_url = models.CharField(null=True, max_length=1000)
+    profile_picture_url = models.CharField(null=True, max_length=1000)
+
+
+class CompanyRole(models.Model):
+    role_name = models.CharField(max_length=50)
+
+
+class CompanyUser(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    role = models.ForeignKey(CompanyRole, on_delete=models.PROTECT)
+
+
+class CompanyChange(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    change_type = models.ForeignKey(ChangeType, on_delete=models.PROTECT)
+    field = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    changed = models.DateTimeField(auto_now_add=True)
+    new_value = models.TextField()
+
+
+class CompanyNames(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    language = models.ForeignKey(Language, on_delete=models.PROTECT)
+    name = models.CharField(max_length=200)
+
+
+class CompanyDescriptions(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    language = models.ForeignKey(Language, on_delete=models.PROTECT)
+    description = models.CharField(max_length=2000)
+
+
+class Country(models.Model):
+    country_iso_3166_1 = models.CharField(max_length=2, null=True)
+
+
+class Address(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    addressee = models.CharField(max_length=100, null=True)
+    co_address = models.CharField(max_length=100, null=True)
+    street_address = models.CharField(max_length=100, null=True)
+    city = models.CharField(max_length=20, null=True)
+    zip_code = models.CharField(max_length=10, null=True)
+    country = models.ForeignKey(Country, on_delete=models.PROTECT)
+
+
+class ContactType(models.Model):
+    contact_type = models.CharField(max_length=100)
+
+
+class Contact(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    contact_type = models.ForeignKey(ContactType, on_delete=models.PROTECT)
+    value = models.CharField(max_length=500)
+    description = models.CharField(max_length=1000, null=True)
+
+
+class Product(models.Model):
+    product_code = models.CharField(max_length=50)
+
+
+class ProductName(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    language = models.ForeignKey(Language, on_delete=models.PROTECT)
+    name = models.CharField(max_length=200)
+
+
+class Currency(models.Model):
+    currency_code = models.CharField(max_length=3)
+
+
+class Order(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    price_per_unit = models.DecimalField(max_digits=20, decimal_places=2, null=True)
+    unit_type = models.CharField(max_length=20, null=True)
+    currency = models.ForeignKey(Currency, on_delete=models.PROTECT)
