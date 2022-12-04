8cb0c2ab0a650872b8d68d587f1e916ba305fd5b
Henrik Hjalmarsson <henrik.hjalmarsson@payex.com>
2022-12-04 17:28:31 +0100

    Working on localization

--- farmers-market/farmers_market/farmers_market/settings.py
+++ farmers-market/farmers_market/farmers_market/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from os.path import join
 from pathlib import Path
 
+from django.utils.translation import gettext_lazy as _
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 CURRENT_DIR = Path(__file__).resolve().parent
 BASE_DIR = CURRENT_DIR.parent
@@ -47,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "django.middleware.locale.LocaleMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -108,7 +111,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'sv-se'
+
+LANGUAGES = [
+    ("sv", _("Swedish")),
+    ("en", _("English")),
+]
 
 TIME_ZONE = 'UTC'
 
