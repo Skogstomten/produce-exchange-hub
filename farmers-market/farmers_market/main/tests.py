71f075cb2295ea289f86d4483bcf2da35b374cdf
Henrik Hjalmarsson <henrik.hjalmarsson@payex.com>
2022-12-07 07:19:49 +0100

    Working on tests of edit company view

--- farmers-market/farmers_market/main/tests.py
+++ farmers-market/farmers_market/main/tests.py
@@ -1,10 +1,43 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
 from . import models
 
 
-def _create_user() -> models.User:
-    return models.User.objects.create_user("nisse", "nisse@persson.se", "test123")
+def _create_user() -> tuple[models.User, str, str]:
+    username = "nisse"
+    password = "test123"
+    return models.User.objects.create_user(username, "nisse@persson.se", password), username, password
+
+
+def _create_company_with_admin() -> tuple[models.Company, models.User, str, str]:
+    company = _create_company()
+    role = _create_company_admin_role()
+    user, username, password = _create_user()
+    models.CompanyUser.objects.create(company=company, role=role, user=user)
+    return company, user, username, password
+
+
+def _create_company_admin_role() -> models.CompanyRole:
+    return models.CompanyRole.objects.create(role_name="company_admin")
+
+
+def _get_or_create(model, selectors, create_fields=None):
+    if not create_fields:
+        create_fields = selectors
+    try:
+        result = model.objects.get(**selectors)
+    except model.DoesNotExist:
+        result = model.objects.create(**create_fields)
+    return result
+
+
+def _get_company_type(name: str) -> models.CompanyType:
+    return _get_or_create(models.CompanyType, {"type_name": name})
+
+
+def _get_language(iso_639_1):
+    return _get_or_create(models.Language, {"iso_639_1": iso_639_1}, {"iso_639_1": iso_639_1, "name": "Whatever"})
 
 
 def _create_company() -> models.Company:
@@ -12,15 +45,32 @@ def _create_company() -> models.Company:
     return models.Company.objects.create(name="Nisses firma", status=status)
 
 
+class EditCompanyViewTest(TestCase):
+    def test_post_returns_202(self):
+        company, _, username, password = _create_company_with_admin()
+
+        client = Client()
+        if not client.login(username=username, password=password):
+            self.fail("Login failed")
+
+        response = client.post(
+            reverse("main:edit_company", kwargs={"pk": company.id}),
+            {
+                "company_name": "Test Company",
+                "company_types": _get_company_type("buyer").id,
+                "content_languages": _get_language("sv").id,
+                "external_website_url": "",
+            }
+        )
+        self.assertEquals(response.status_code, 202)
+
+
 class CompanyModelTest(TestCase):
     def test_is_company_admin_user_is_admin(self):
-        user = _create_user()
-        role = models.CompanyRole.objects.create(role_name="company_admin")
-        company = _create_company()
-        models.CompanyUser.objects.create(company=company, user=user, role=role)
+        company, user, _, _ = _create_company_with_admin()
 
         company = models.Company.objects.get(pk=company.id)
         self.assertTrue(company.is_company_admin(user))
 
     def test_is_company_admin_user_is_not_admin(self):
-        self.assertFalse(_create_company().is_company_admin(_create_user()))
+        self.assertFalse(_create_company().is_company_admin(_create_user()[0]))
--- farmers-market/farmers_market/main/views.py
+++ farmers-market/farmers_market/main/views.py
@@ -1,5 +1,6 @@
 """Views for main module"""
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
 from django.http import HttpRequest, HttpResponseForbidden
 from django.views.generic import View
 from django.conf import settings
@@ -33,19 +34,64 @@ class EditCompanyView(LoginRequiredMixin, View):
     def get(self, request: HttpRequest, pk: int):
         company = get_object_or_404(models.Company, pk=pk)
         content_languages = [language.iso_639_1 for language in company.content_languages.all()]
-        if company.is_company_admin(request.user):
-            return render(
-                request,
-                "main/edit_company.html",
-                {
-                    "company": company,
-                    "company_types": models.CompanyType.objects.all(),
-                    "languages": [
-                        {"id": language.id, "name": language.name, "checked": language.iso_639_1 in content_languages}
-                        for language in models.Language.objects.all()
-                    ],
-                },
-            )
+        company_types = [company_type.type_name for company_type in company.company_types.all()]
 
-        # TODO: Make a better forbidden page
-        return HttpResponseForbidden()
+        if not company.is_company_admin(request.user):
+            return HttpResponseForbidden()  # TODO: Make better
+
+        return render(
+            request,
+            "main/edit_company.html",
+            {
+                "company": company,
+                "company_types": [
+                    {
+                        "id": company_type.id,
+                        "type_name": company_type.type_name,
+                        "checked": "checked" if company_type.type_name in company_types else "",
+                    }
+                    for company_type in models.CompanyType.objects.all()
+                ],
+                "languages": [
+                    {
+                        "id": language.id,
+                        "name": language.name,
+                        "checked": "checked" if language.iso_639_1 in content_languages else "",
+                    }
+                    for language in models.Language.objects.all()
+                ],
+            },
+        )
+
+    def post(self, request: HttpRequest, pk: int):
+        company = get_object_or_404(models.Company, pk=pk)
+        company.name = request.POST.get("company_name").strip()
+        company.external_website_url = request.POST.get("external_website_url").strip()
+
+        current_company_type_ids = set(company_type.id for company_type in company.company_types.all())
+        selected_company_type_ids = set(
+            int(company_type_id) for company_type_id in request.POST.getlist("company_types")
+        )
+
+        for current_company_type_id in current_company_type_ids:
+            if not current_company_type_id in selected_company_type_ids:
+                company.company_types.remove(models.CompanyType.objects.get(pk=current_company_type_id))
+
+        for selected_company_type_id in selected_company_type_ids:
+            if not selected_company_type_id in current_company_type_ids:
+                company.company_types.add(models.CompanyType.objects.get(pk=selected_company_type_id))
+
+        current_language_ids = set(language.id for language in company.content_languages.all())
+        selected_language_ids = set(int(language_id) for language_id in request.POST.getlist("content_languages"))
+
+        for current_language_id in current_language_ids:
+            if not current_language_id in selected_language_ids:
+                company.content_languages.remove(models.Language.objects.get(current_language_id))
+
+        for selected_language_id in selected_language_ids:
+            if not selected_language_id in current_language_ids:
+                company.content_languages.add(models.Language.objects.get(pk=selected_company_type_id))
+
+        company.save()
+
+        return redirect(reverse("main:edit_company", kwargs={"pk": pk}))
