04a7360616048afa6d485bede8033e500224ab03
Henrik Hjalmarsson <henrik.hjalmarsson@payex.com>
2022-12-13 21:01:45 +0100

    Added company desc to index

--- farmers-market/farmers_market/main/views.py
+++ farmers-market/farmers_market/main/views.py
@@ -27,6 +27,8 @@ class CompanyProfilePictureView(View):
 
 def index(request: HttpRequest):
     companies = Company.objects.filter(status__status_name="active").order_by("-activation_date")[:10]
+    for company in companies:
+        company.description = company.get_description(get_language(request))
     return render(request, "main/index.html", {"companies": companies})
 
 
