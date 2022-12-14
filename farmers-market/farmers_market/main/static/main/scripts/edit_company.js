353595a57e91b3e890846f13a75a6f99a571d61f
Henrik Hjalmarsson <henrik.hjalmarsson@payex.com>
2022-12-14 18:27:25 +0100

    Displays crop modal when selecting picture

--- farmers-market/farmers_market/main/static/main/scripts/edit_company.js
+++ farmers-market/farmers_market/main/static/main/scripts/edit_company.js
@@ -1,50 +1,23 @@
 (function() {
     $("#id_profile_picture").on("change", function() {
         if (this.files && this.files[0]) {
+            const image = $("#image");
             const reader = new FileReader();
-            reader.onload = function (e) {
-                $("#image").attr("src", e.target.result);
-                $("#modal_crop").modal("show");
+            reader.onload = function(e) {
+                image.attr("src", e.target.result);
             };
-            reader.readAsDataURL(this.files[0]);
+            reader.readAsDataURL(this.files[0]); 
+
+            $("#model_crop_image").modal("show");
         }
     });
 
-    const $image = $("#image");
-    let cropBoxData;
-    let canvasData;
-    $("#modal_crop").on("show.bs.modal", function() {
-        $image.cropper({
-            viewMode: 1,
-            aspectRatio: 1/1,
-            minCropBoxWidth: 200,
-            minCropBoxHeight: 200,
-            ready: function () {
-                $image.cropper("setCanvasData", canvasData);
-                $image.cropper("setCropBoxData", cropBoxData);
-            }
+    $("#model_crop_image").on("shown.bs.modal", function() {
+        const cropper = new Cropper(image, {
+            aspectRatio: 16 / 9,
+            crop(event) {
+                console.log(event);
+            },
         });
-    }).on("hidden.bs.modal", function() {
-        cropBoxData = $image.cropper("getCropBoxData");
-        canvasData = $image.cropper("getCanvasData");
-        $image.cropper("destroy")
-    });
-
-    $(".js-zoom-in").click(function () {
-        $image.cropper("zoom", 0.1);
-    });
-
-    $(".js-zoom-out").click(function () {
-        $image.cropper("zoom", -0.1);
-    });
-
-    /* SCRIPT TO COLLECT THE DATA AND POST TO THE SERVER */
-    $(".js-crop-and-upload").click(function () {
-        let cropData = $image.cropper("getData");
-        $("#id_x").val(cropData["x"]);
-        $("#id_y").val(cropData["y"]);
-        $("#id_height").val(cropData["height"]);
-        $("#id_width").val(cropData["width"]);
-        $("#formUpload").submit();
     });
 }());
