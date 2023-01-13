c0cf7d10d16eb64dca8344c4b37eb9e1843d2192
Henrik Hjalmarsson <henrik.hjalmarsson@payex.com>
2023-01-14 00:08:32 +0100

    Added table with companies on user.

--- farmers-market/farmers_market/main/templatetags/company_tags.py
+++ farmers-market/farmers_market/main/templatetags/company_tags.py
@@ -5,4 +5,6 @@ register = Library()
 
 @register.inclusion_tag("main/tag_templates/user_companies.html")
 def user_companies(user: User):
-    return user.companies.company
+    return {
+        "companies": [company_user.company for company_user in user.companies.all()],
+    }
