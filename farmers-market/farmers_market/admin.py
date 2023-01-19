5a2491b50e85eee76105c62b64a2debe8b91daad
Henrik Hjalmarsson <Skogstomten@users.noreply.github.com>
2023-01-19 08:59:22 +0100

    Working on admin.py

--- farmers-market/farmers_market/admin.py
+++ farmers-market/farmers_market/admin.py
@@ -6,17 +6,23 @@ LINE_LENGTH = 121
 
 
 def run_subprocess(args: list, working_dir: str | None = None):
-    process = subprocess.run(args, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=working_dir)
+    process = subprocess.run(
+        args, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=working_dir
+    )
     print(process.stdout)
 
 
 def run_django_admin_command(commands, working_dir):
     run_subprocess(["django-admin.exe"] + commands, working_dir)
 
+
 def run_black():
     run_subprocess(["black", "./", f"--line-length={LINE_LENGTH}"])
 
-def commit_all_changes()
+
+def commit_all_changes(*messages):
+    run_subprocess(["git", "add", "-A"])
+    run_subprocess(["git", "commit"] + [f'-m"{message}"' for message in messages])
 
 
 def make_messages(subdir):
@@ -34,13 +40,16 @@ def for_each_subdir(func):
         func(subdir)
 
 
-def format_and_commit():
-    pass
+def format_and_commit(*messages):
+    run_black()
+    print(messages)
+    # commit_all_changes(messages)
 
 
 commands = {
     "makemessages": (for_each_subdir, (make_messages,)),
     "compilemessages": (for_each_subdir, (compile_messages,)),
+    "commit": (format_and_commit, ()),
 }
 
 
@@ -49,8 +58,9 @@ if __name__ == "__main__":
         print("Missing command")
     else:
         command = sys.argv[1].lower()
+        additional_arguments = sys.argv[2:]
         try:
             func, args = commands[command]
-            func(*args)
+            func(*args, *additional_arguments)
         except KeyError:
             print("Unsupported command. Supported commands are: {}".format(", ".join([k for k in commands.keys()])))
