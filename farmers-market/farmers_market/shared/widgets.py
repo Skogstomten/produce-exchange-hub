6bfe00f1b8092cbb6f17dffd203607f0b9fac7f7
Henrik Hjalmarsson <henrik.hjalmarsson@payex.com>
2023-01-26 09:03:34 +0100

    "Working on searchable select widget"

--- farmers-market/farmers_market/shared/widgets.py
+++ farmers-market/farmers_market/shared/widgets.py
@@ -1,5 +1,17 @@
+from typing import Mapping, Callable
+
 from django.forms.widgets import ChoiceWidget
+from django.db.models import QuerySet, Model
+from django.db.models.fields.related import RelatedField
 
 
 class SearchableSelectWidget(ChoiceWidget):
     template_name = "/shared/widget_templates/searchable_select_input.html"
+
+    def __init__(
+        self,
+        dataset: QuerySet | RelatedField,
+        get_display_value: Callable[[Model], str],
+        attrs: Mapping[str, str] = None,
+    ):
+        super().__init__(attrs, [(entity.id, get_display_value(entity)) for entity in dataset])
