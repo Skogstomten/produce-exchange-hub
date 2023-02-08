c5ce0225ee91b5d6da8c9c560f0f7a9dd2317c3b
Henrik Hjalmarsson <Skogstomten@users.noreply.github.com>
2023-02-08 21:36:45 +0100

    "Continued work on js for searchable dropdown"

--- farmers-market/farmers_market/farmers_market/static/farmers_market/scripts/searchable_select.js
+++ farmers-market/farmers_market/farmers_market/static/farmers_market/scripts/searchable_select.js
@@ -1,14 +1,29 @@
 (function(){
     const searchableDropdowns = document.getElementsByClassName("searchable-dropdown");
+
+    function addCreateNewEventHandler(el) {
+        el.onclick = function(event) {
+            alert("It's clicked!");
+        };
+    }
+
     for (const el of searchableDropdowns) {
-        const input = el.getElementsByTagName("input")[0]
-        const content = el.getElementsByClassName("dropdown-content")[0]
+        const input = el.getElementsByTagName("input")[0];
+        const content = el.getElementsByClassName("dropdown-content")[0];
         input.onfocus = function () {
-            content.classList.toggle("show")
+            options = content.getElementsByTagName("div");
+            if (options.length < 1) {
+                let addItemNode = document.createElement("div");
+                addItemNode.appendChild(document.createTextNode("Create new"));
+                addCreateNewEventHandler(addItemNode);
+                content.appendChild(addItemNode);
+            }
+
+            content.classList.toggle("show");
         };
 
         input.onchange = function() {
-            const options = content.getElementsByTagName("div")
+            const options = content.getElementsByTagName("div");
             for (const option of options) {
                 const text = option.innerText;
             }
